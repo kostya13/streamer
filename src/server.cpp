ac07f2f4f31953a17069811ef033ee33f497f952
Konstantin Ilyashenko <konstantin@gigaspaces.com>
2017-09-28 14:23:46 +0400

    Рефакторинг сервера

--- src/server.cpp
+++ src/server.cpp
@@ -1,5 +1,15 @@
 /*
-    Simple udp server
+    UDP сервер.
+	Принимает поток пакетов
+	Ведет подсчет принятым пакетам.
+	Если обнаруживает пропущенные пакеты,
+	посылает запрос к клиенту на повторную передачу пакетов
+
+	В данном примере полезная нагрузка пакета никак не обрабатывается.
+
+	Константин Ильяшенко
+	Сентябрь 2017
+
 */
 #include <algorithm>
 #include <iostream>
@@ -13,49 +23,79 @@
 const unsigned int BUFLEN =  1028;
 const unsigned int PORT = 8888;
 const unsigned int SEND_BUF_LEN = 2;
- 
+const socklen_t SLEN = sizeof(sockaddr_in);
+typedef std::unordered_set<uint32_t> Missed;
+
 void die(const char *s)
 {
     perror(s);
     exit(1);
 }
  
+void handle_new_packets(uint32_t counter, uint32_t last_counter, int socket, sockaddr_in* si_other,
+					    Missed& missed_packets)
+{
+  bool has_missed_packets = (counter - last_counter > 1);
+  uint32_t out_buf[SEND_BUF_LEN];
+  if(has_missed_packets)
+  {
+	out_buf[0] = ntohl(last_counter + 1);
+	out_buf[1] = ntohl(counter);
+	if (sendto(socket, out_buf, sizeof(out_buf), 0, (sockaddr*) si_other, SLEN) == -1)
+	{
+	  std::cout<<"Send error"<<std::endl;
+	  return;
+	}
+	for(auto i = last_counter + 1; i < counter; i++)
+	{
+	  std::cout<<"Skipped: "<<i<<std::endl;
+	  missed_packets.insert(i);
+	} 
+  }
+}
+
+
+int setup_socket()
+{
+  int s;
+  sockaddr_in si_me;
+  if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
+  {
+	die("socket");
+  }
+
+  memset((char*)&si_me, 0, sizeof(si_me));
+  si_me.sin_family = AF_INET;
+  si_me.sin_port = htons(PORT);
+  si_me.sin_addr.s_addr = htonl(INADDR_ANY);
+
+  if( bind(s, (sockaddr*)&si_me, sizeof(si_me) ) == -1)
+  {
+	die("bind");
+  }
+  return s;
+}
+
 int main(void)
 {
-    struct sockaddr_in si_me, si_other;
-     
-    int s, i, recv_len;
-	socklen_t slen = sizeof(si_other);
+    sockaddr_in si_other;
+    socklen_t slen = sizeof(sockaddr_in);
+	int socket = setup_socket();
     char buf[BUFLEN];
-     
-    if ((s=socket(AF_INET, SOCK_DGRAM, IPPROTO_UDP)) == -1)
-    {
-        die("socket");
-    }
-     
-    memset((char *) &si_me, 0, sizeof(si_me));
-    si_me.sin_family = AF_INET;
-    si_me.sin_port = htons(PORT);
-    si_me.sin_addr.s_addr = htonl(INADDR_ANY);
-     
-    if( bind(s , (struct sockaddr*)&si_me, sizeof(si_me) ) == -1)
-    {
-        die("bind");
-    }
-     
 	uint32_t last_counter = 0;
-	std::unordered_set<uint32_t> missed_packets;
-
-	if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+	Missed missed_packets;
+     
+	if (recvfrom(socket, buf, BUFLEN, 0, (sockaddr*)&si_other, &slen) == -1)
 	{
 	  die("Recieve first packet");
 	}
 	uint32_t counter = ntohl(reinterpret_cast<uint32_t&>(buf));
 	std::cout<<"First packet: "<<counter<<std::endl;
 	last_counter = counter;
+
     while(1)
 	{
-	  if ((recv_len = recvfrom(s, buf, BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == -1)
+	  if (recvfrom(socket, buf, BUFLEN, 0, (sockaddr*)&si_other, &slen) == -1)
 	  {
 		std::cout<<"Recieve error"<<std::endl;
 		continue;
@@ -66,23 +106,7 @@ int main(void)
 	  bool new_packet = (missed == missed_packets.end());
 	  if(new_packet)
 	  {
-		bool has_missed_packets = (counter - last_counter > 1);
-		if(has_missed_packets)
-		{
-		  uint32_t out_buf[SEND_BUF_LEN];
-		  out_buf[0] = ntohl(last_counter + 1);
-		  out_buf[1] = ntohl(counter);
-		  if (sendto(s,  out_buf, sizeof(uint32_t) * SEND_BUF_LEN , 0, (struct sockaddr*) &si_other, slen) == -1)
-		  {
-			std::cout<<"Send error"<<std::endl;
-			continue;
-		  }
-		  for(uint32_t i = last_counter + 1; i < counter; i++)
-		  {
-			  std::cout<<"Skipped: "<<i<<std::endl;
-			  missed_packets.insert(i);
-		  } 
-		}
+		handle_new_packets(counter, last_counter, socket, &si_other, missed_packets);
 		last_counter = counter;
 	  }
 	  else
